2a5b3fb2e65e93f20df899d8abaa4d8844773e06
ShinkuKira21 <crazygaming055@gmail.com>
2021-04-22 17:51:46 +0100

    Added API connection

---
+++ E2T/Eth2Tracker/API.py
@@ -0,0 +1,10 @@
+import requests
+class API :
+    def __init__(self, url) :
+        self.response = None
+        self.url = url
+    def Connect(self):
+        self.response = requests.get(self.url)
+
+    def Result(self):
+        return self.response.json()
--- E2T/Eth2Tracker/Eth2Tracker.py
+++ E2T/Eth2Tracker/Eth2Tracker.py
@@ -1,7 +1,18 @@
 from timer import timer
 from DatabaseConnection import DatabaseConnection
+from API import API
 
 dbc = DatabaseConnection()
+
+currentStats = API("https://api.ethermine.org/miner/d2B0B8133b1E30EC7C7936153116F8bC955cb20f/currentStats")
+
+currentStats.Connect()
+eth = float(currentStats.Result()['data']['coinsPerMin'])
+eth *= 60
+eth *= 24
+eth *= 7
+eth *= 4
+print(str(format(eth, '.5f')) + "eth")
 
 class Task :
     def executeTask(self) :
