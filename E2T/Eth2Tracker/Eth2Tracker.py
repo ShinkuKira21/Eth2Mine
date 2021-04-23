60dab990f84879fbb8779574babaa66ca25ad61c
ShinkuKira21 <crazygaming055@gmail.com>
2021-04-23 18:00:43 +0100

    Added Config and Task Problem

--- E2T/Eth2Tracker/Eth2Tracker.py
+++ E2T/Eth2Tracker/Eth2Tracker.py
@@ -1,12 +1,15 @@
+from config import Config
 from timer import timer
 from DatabaseConnection import DatabaseConnection
 import tasks
 
+conf = Config()
 dbc = DatabaseConnection()
 
 class Task :
     def executeTask(self) :
-        print(str(tasks.GetCurrentThreshold()) + "eth")
+        print(str(tasks.GetCurrentThreshold(conf.primaryWallet)) + "eth")
+        tasks.SetPot(conf.primaryWallet, conf.units)
 
 # 10 Minutes
 timer = timer(Task())
--- E2T/Eth2Tracker/tasks.py
+++ E2T/Eth2Tracker/tasks.py
@@ -1,7 +1,7 @@
 from API import API
 
-def GetCurrentThreshold () :
-    currentStats = API("https://api.ethermine.org/miner/d2B0B8133b1E30EC7C7936153116F8bC955cb20f/currentStats")
+def GetCurrentThreshold (wallet) :
+    currentStats = API("https://api.ethermine.org/miner/" + wallet + "/currentStats")
 
     currentStats.Connect()
     eth = currentStats.Result()['data']['coinsPerMin']
@@ -13,3 +13,20 @@ def GetCurrentThreshold () :
 
     return float(format(eth, '.5f'))
     
+def SetPot(wallet, units) :
+    dashboard = API("https://api.ethermine.org/miner/" + wallet + "/dashboard")
+    dashboard.Connect()
+
+    currentPot = dashboard.Result()['data']['currentStatistics']['unpaid']
+
+    # Create a new record with new ammount (problem here, if current pot is like 0.0001 
+    # when it next checks 10 mins downline, then it won't make new record. Also if < 0.001ETH,
+    # 10 minutes down line might still be 0.001 ETH (not likely but could be possible) so would
+    # make another record, when it should just update... would this work?
+    #  (currentPot == 0 || currentPot < 0.001) && currentPot != lastPot
+    if(currentPot == 0) :
+        pass
+
+    # Update current record (as ethermine has not reset)
+    else :
+        pass
