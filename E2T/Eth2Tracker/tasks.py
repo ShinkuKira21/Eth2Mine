342565f16b2198a056cde936369bd3672ea5beff
ShinkuKira21 <crazygaming055@gmail.com>
2021-04-25 20:25:53 +0100

    Update

--- E2T/Eth2Tracker/tasks.py
+++ E2T/Eth2Tracker/tasks.py
@@ -31,10 +31,11 @@ def SetPot(wallet, units) :
     apiResults = apiGetLatestMiningStats(wallet)
     
     unpaid = apiResults['data']['currentStatistics']['unpaid'] / units
-    totalUP = float(results[0]) + unpaid; totalUP = format(totalUP, '.8f')
-    totalTP = float(results[1]); totalTP = format(totalTP, '.8f')
 
-    qry = "INSERT INTO Pool(ID, totalMined, totalPayout, timestamp) VALUES(" + str(index) + ", " + str(totalUP) + ", " + str(totalTP) + ", '" + str(currentDateTime) + "')"
+    totalUP = (float(results[0]) + unpaid) - float(results[2]); totalUP = format(totalUP, '.8f')
+    totalTP = float(results[1]); totalTP = format(totalTP, '.8f')
+  
+    qry = "INSERT INTO Pool(ID, totalMined, totalPayout, unpaid, timestamp) VALUES(" + str(index) + ", " + str(totalUP) + ", " + str(totalTP) + ", " + str(unpaid) + ", '" + str(currentDateTime) + "')"
 
     dbc.QuerySelectDatabase(qry)
     status = dbc.CommitDatabase()
@@ -43,7 +44,7 @@ def SetPot(wallet, units) :
     return status
 
 def GetLatestPool() :
-    qry = "SELECT totalMined, totalPayout FROM Pool"
+    qry = "SELECT totalMined, totalPayout, unpaid FROM Pool"
 
     dbc.ConnectToDatabase()
     cursor = dbc.QuerySelectDatabase(qry)
@@ -52,7 +53,7 @@ def GetLatestPool() :
 
     for row in cursor :
         results = []
-        for i in range(0, 2) :
+        for i in range(0, 3) :
             results.append(str(row[i]))
 
     return results
